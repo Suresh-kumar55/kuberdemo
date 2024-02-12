1266ec46fba32a969393f0182aa17bb2db91c818
Suresh-kumar55 <sureshsuryakumar55@gmail.com>
2024-02-12 19:19:47 +0530

    first commit

---
+++ springbootkuber-demo/src/main/java/om/star/agile/assignment/springbootkuberdemo/SpringbootkuberDemoApplication.java
@@ -0,0 +1,13 @@
+package om.star.agile.assignment.springbootkuberdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootkuberDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootkuberDemoApplication.class, args);
+	}
+
+}
---
+++ springbootkuber-demo/src/test/java/om/star/agile/assignment/springbootkuberdemo/SpringbootkuberDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package om.star.agile.assignment.springbootkuberdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootkuberDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
